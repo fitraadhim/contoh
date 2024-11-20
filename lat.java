5a46c44047351540ddab6e11a5be577fc9caf7f2
fitraadhim <azzazria123@gmail.com>
2024-11-20 14:49:12 +0700

    aw

---
+++ lat.java
@@ -0,0 +1,15 @@
+public class lat {
+    public static void main(String[] args) {
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    System.out.println("contoh123");        
+    }
+}
